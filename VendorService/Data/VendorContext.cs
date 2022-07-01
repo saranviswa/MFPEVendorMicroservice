e46fe19fa86d0aa29d15719705e6f688a282c882
saran viswa <bsaranviswa@gmail.com>
2022-07-01 19:19:34 +0530

    first commit

---
+++ VendorService/Data/VendorContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using VendorService.Models;
+
+namespace VendorService.Data
+{
+    public class VendorContext : DbContext
+    {
+        public VendorContext(DbContextOptions opts) : base(opts)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VendorStock>().HasData(
+                   new VendorStock() { Id = 1, ProductId = 1, VendorId = 201, HandInStocks = 24, ReplinshmentDate = Convert.ToDateTime(" 2022 - 06 - 07") },
+                   new VendorStock() { Id = 2, ProductId = 3, VendorId = 202, HandInStocks = 24, ReplinshmentDate = Convert.ToDateTime(" 2022 - 06 - 07") },
+                   new VendorStock() { Id = 3, ProductId = 4, VendorId = 201, HandInStocks = 24, ReplinshmentDate = Convert.ToDateTime(" 2022 - 06 - 07") },
+                   new VendorStock() { Id = 4, ProductId = 5, VendorId = 202, HandInStocks = 24, ReplinshmentDate = Convert.ToDateTime(" 2022 - 06 - 07") }
+               );
+            modelBuilder.Entity<Vendor>().HasData(
+                  new Vendor() { Id = 201, Name = "DelhiMotoShop", DeliveryCharge = 45, Rating = 5 },
+                  new Vendor() { Id = 202, Name = "HydMotoShop", DeliveryCharge = 50, Rating = 4 }
+              );
+        }
+        public DbSet<Vendor> Vendor { get; set; }
+        public DbSet<VendorStock> VendorStock { get; set; }
+    }
+}
---
+++ VendorService/Migrations/20220628192210_intial.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VendorService.Migrations
+{
+    public partial class intial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vendor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DeliveryCharge = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VendorStock",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    VendorId = table.Column<int>(type: "int", nullable: false),
+                    HandInStocks = table.Column<int>(type: "int", nullable: false),
+                    ReplinshmentDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VendorStock", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VendorStock_Vendor_VendorId",
+                        column: x => x.VendorId,
+                        principalTable: "Vendor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Vendor",
+                columns: new[] { "Id", "DeliveryCharge", "Name", "Rating" },
+                values: new object[] { 201, 45, "DelhiMotoShop", 5 });
+
+            migrationBuilder.InsertData(
+                table: "Vendor",
+                columns: new[] { "Id", "DeliveryCharge", "Name", "Rating" },
+                values: new object[] { 202, 50, "HydMotoShop", 4 });
+
+            migrationBuilder.InsertData(
+                table: "VendorStock",
+                columns: new[] { "Id", "HandInStocks", "ProductId", "ReplinshmentDate", "VendorId" },
+                values: new object[,]
+                {
+                    { 1, 24, 1, new DateTime(2022, 6, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), 201 },
+                    { 3, 24, 4, new DateTime(2022, 6, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), 201 },
+                    { 2, 24, 3, new DateTime(2022, 6, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), 202 },
+                    { 4, 24, 5, new DateTime(2022, 6, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), 202 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VendorStock_VendorId",
+                table: "VendorStock",
+                column: "VendorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VendorStock");
+
+            migrationBuilder.DropTable(
+                name: "Vendor");
+        }
+    }
+}
